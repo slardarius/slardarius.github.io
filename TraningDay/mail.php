71cb0033338e06c9940b21ce744a4f9137c3e800
Arts1r <justtalkabout2@gmail.com>
2017-09-24 17:44:45 +0300

    TraningDay

---
+++ TraningDay/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "justtalkabout2@gmail.com";
+$sitename = "УЧЕБНЫЙ АДАПТИВНЫЙ САЙТ №1";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
--- barberShop/slider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-		$(".Menu dd").hide().prev().click(function(){
-		$(".Menu dd").not(this).slideUp();
-		$(this).next().not(":visible").slideDown();
-		
-	});
-});
