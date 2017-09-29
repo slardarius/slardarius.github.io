cd80201a837ed48428c8df09df183e89f5edcceb
Arts1r <justtalkabout2@gmail.com>
2017-09-29 16:45:34 +0300

    Правки в макет DeltaDIgital

--- DeltaDigital/js/common.js
+++ DeltaDigital/js/common.js
@@ -1,5 +1,5 @@
 (function(){
-
+    /* TODO: WORK WITH BLOCKS */
     var blocks = document.getElementsByClassName('content__items__information');
     var button = document.getElementsByClassName('content__items__button');
     for(var i =0; i < blocks.length; i++){
@@ -10,13 +10,30 @@
     window.onclick = function(e){
         var elem = e.target;
         blocks[elem.id].classList.toggle('show');
+        if(!blocks[elem.id].classList.contains('show') && elem.classList.contains('content__items__button')){
+            elem.innerHTML = 'скрыть';
+        }
+        if(blocks[elem.id].classList.contains('show') && elem.classList.contains('content__items__button')){
+            elem.innerHTML = 'подробнее';
+        }
     }
-    
+
+    /* TOOD: WORK WIDTH BURGER */
     var gamburger = document.getElementById('important');
-    gamburger.onclick = function(){
+    var span_center = document.getElementsByClassName('span_center');
     var menu = document.getElementsByClassName('wrapper__items');
-    for(var i = 0; i < menu.length; i++){
-      menu[i].classList.toggle('show');
+    var ext = document.getElementsByClassName('ext');
+    gamburger.onclick = function(){
+    /* TODO: SWOH MENU */
+        for(var i = 0; i < menu.length; i++){
+        menu[i].classList.toggle('show');
+    }
+    /*TODO: CHANGE GAMURGER ON EXT */
+    span_center[0].classList.toggle('switch');
+    gamburger.classList.toggle('switch');
+    gamburger.style.transition = 'all .5s ease 0.3s';
+    for(var i=0; i < ext.length; i++){
+        ext[i].classList.toggle('switch');
     }
 }
 
