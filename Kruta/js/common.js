953310189b79a82a663b37ca4d9072d3dc79da0b
Arts1r <justtalkabout2@gmail.com>
2017-10-14 00:37:48 +0300

    Version 0.2
    Adaptive

--- Kruta/js/common.js
+++ Kruta/js/common.js
@@ -1,4 +1,35 @@
-(function(){
-    
-   
-})();
+$(document).ready(function(){
+
+    var owl_menu = $("#Carusel_Menu");
+    var owl_reviews = $('#Carusel_Reviews');
+    owl_reviews.owlCarousel({
+        center:true,
+        items: 1
+    });
+    $('.next_slide_reviews').click(function() {
+        owl_reviews.trigger('next.owl.carousel');
+    })
+    $('.prev_slide_reviews').click(function() {
+        owl_reviews.trigger('prev.owl.carousel', [300]);
+    })
+    owl_menu.owlCarousel({
+        responsiveClass:true,
+        nav: false,
+        responsive:{
+            0:{
+                items:1,
+                center:true,
+            },
+            600:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+    $('.next_slide_menu').click(function() {
+        owl_menu.trigger('next.owl.carousel');
+    })
+    $('.prev_slide_menu').click(function() {
+        owl_menu.trigger('prev.owl.carousel', [300]);
+    })
+  });
